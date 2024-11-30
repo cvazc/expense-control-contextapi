a7cab82d2c65468b8413d116282da49e71883bc7
secontreras <se.contreras@mavi.mx>
2024-11-29 19:15:04 -0600

    add BudgetForm component and update App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-export default function App() {
+import BudgetForm from "./components/BudgetForm";
 
-  return (
-    <>
-      <h1>Control de Gastos</h1>
-    </>
-  )
+export default function App() {
+    return (
+        <>
+            <header className="bg-blue-600 py-8 max-h-72">
+                <h1 className="uppercase text-center font-black text-4xl text-white">
+                    Planificador de Gastos
+                </h1>
+            </header>
+            <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+                <BudgetForm />
+            </div>
+        </>
+    );
 }

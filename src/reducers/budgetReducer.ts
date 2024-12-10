e77f4ecbbe59a6f86551a3c6df8e406d8688424c
secontreras <se.contreras@mavi.mx>
2024-12-09 19:23:15 -0600

    feat: implement ExpenseModal with modal state management; add ExpenseForm component; update budgetReducer to handle modal actions

--- src/reducers/budgetReducer.ts
+++ src/reducers/budgetReducer.ts
@@ -1,12 +1,17 @@
-export type BudgetActions = { type: "add-budget"; payload: { budget: number } };
+export type BudgetActions =
+    | { type: "add-budget"; payload: { budget: number } }
+    | { type: "show-modal" } 
+    | { type: "close-modal" } 
 
 export type BudgetState = {
-    budget: number;
-};
+    budget: number
+    modal: boolean
+}
 
 export const initialState: BudgetState = {
     budget: 0,
-};
+    modal: false,
+}
 
 export const budgetReducer = (
     state: BudgetState = initialState,
@@ -16,8 +21,22 @@ export const budgetReducer = (
         return {
             ...state,
             budget: action.payload.budget,
-        };
+        }
     }
 
-    return state;
-};
+    if (action.type === "show-modal") {
+        return {
+            ...state,
+            modal: true,
+        }
+    }
+
+    if (action.type === "close-modal") {
+        return {
+            ...state,
+            modal: false,
+        }
+    }
+
+    return state
+}

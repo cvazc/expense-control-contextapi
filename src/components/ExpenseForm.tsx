c7186ba31edcdb8a93409d7a0b1b832dc2cf6e17
secontreras <se.contreras@mavi.mx>
2024-12-10 18:49:12 -0600

    feat: enhance ExpenseForm with input fields for expense name, amount, and category selection; add categories data

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,6 +1,61 @@
+import { categories } from "../data/categories"
 
 export default function ExpenseForm() {
-  return (
-    <div>ExpenseForm</div>
-  )
+    return (
+        <form className="space-y-5">
+            <legend className="upperacase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
+                Nuevo Gasto
+            </legend>
+
+            <div className="flex flex-col">
+                <label htmlFor="expenseName" className="text-xl">
+                    Nombre Gasto:
+                </label>
+                <input
+                    type="text"
+                    id="expenseName"
+                    placeholder="Agrega el nombre del gasto"
+                    className="bg-slate-100 p-2"
+                    name="expenseName"
+                />
+            </div>
+
+            <div className="flex flex-col">
+                <label htmlFor="amount" className="text-xl">
+                    Cantidad:
+                </label>
+                <input
+                    type="number"
+                    id="amount"
+                    placeholder="Agrega la cantidad del gasto ej. 300"
+                    className="bg-slate-100 p-2"
+                    name="amount"
+                />
+            </div>
+
+            <div className="flex flex-col">
+                <label htmlFor="categorie" className="text-xl">
+                    Categoria:
+                </label>
+                <select
+                    id="categorie"
+                    className="bg-slate-100 p-2"
+                    name="categorie"
+                >
+                    <option value="">-- Seleccione --</option>
+                    {categories.map((category) => (
+                        <option key={category.id} value={category.id}>
+                            {category.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <input
+                type="submit"
+                className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
+                value="Registrar Gasto"
+            />
+        </form>
+    )
 }

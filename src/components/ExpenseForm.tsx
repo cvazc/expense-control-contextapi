d5c6b1eef7c38644acccc73134ed3d6fd31b0bf4
secontreras <se.contreras@mavi.mx>
2024-12-11 19:14:05 -0600

    feat: implement expense submission handling in ExpenseForm; add error message display and update budgetReducer to manage expenses

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,9 +1,11 @@
-import { useState, ChangeEvent } from "react"
+import { useState, ChangeEvent, FormEvent } from "react"
 import type { DraftExpense, Value } from "../types"
 import { categories } from "../data/categories"
 import DatePicker from "react-date-picker"
 import "react-calendar/dist/Calendar.css"
 import "react-date-picker/dist/DatePicker.css"
+import ErrorMessage from "./ErrorMessage"
+import { useBudget } from "../hooks/useBudget"
 
 export default function ExpenseForm() {
     const [expense, setExpense] = useState<DraftExpense>({
@@ -12,6 +14,10 @@ export default function ExpenseForm() {
         category: "",
         date: new Date(),
     })
+
+    const [error, setError] = useState("")
+
+    const { dispatch } = useBudget()
 
     const handleChange = (
         e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
@@ -31,11 +37,30 @@ export default function ExpenseForm() {
         })
     }
 
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        if (Object.values(expense).includes("")) {
+            setError("Todos los campos son obligatorios")
+            return
+        }
+
+        dispatch({ type: "add-expense", payload: { expense } })
+
+        setExpense({
+            amount: 0,
+            expenseName: "",
+            category: "",
+            date: new Date(),
+        })
+    }
+
     return (
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={handleSubmit}>
             <legend className="upperacase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
                 Nuevo Gasto
             </legend>
+
+            {error && <ErrorMessage>{error}</ErrorMessage>}
 
             <div className="flex flex-col">
                 <label htmlFor="expenseName" className="text-xl">

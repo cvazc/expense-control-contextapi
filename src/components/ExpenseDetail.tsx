8f26ee780744f0f654be2156902365fb6e278002
secontreras <se.contreras@mavi.mx>
2024-12-12 18:47:29 -0600

    feat: enhance ExpenseDetail component with category information and date formatting; add formatDate helper function

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,11 +1,38 @@
+import { useMemo } from "react"
+import { categories } from "../data/categories"
+import { formatDate } from "../helpers"
 import { Expense } from "../types"
+import AmountDisplay from "./AmountDisplay"
 
 type ExpenseDetailProps = {
     expense: Expense
 }
 
-export default function ExpenseDetail({expense} : ExpenseDetailProps) {
-  return (
-    <div>ExpenseDetail</div>
-  )
+export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
+    const categoryInformation = useMemo(
+        () => categories.filter((cat) => cat.id === expense.category)[0],
+        [expense]
+    )
+    
+    return (
+        <div className="bg-white shadow-lg w-full border-b border-gray-200 flex gap-5 items-center">
+            <div className="">
+                <img
+                    src={`/icono_${categoryInformation.icon}.svg`}
+                    alt="Icono Gasto"
+                    className="w-20"
+                />
+            </div>
+            <div className="flex-1 space-y-2">
+                <p className="text-sm font-bold uppercase text-slate-500">
+                    {categoryInformation.name}
+                </p>
+                <p>{expense.expenseName}</p>
+                <p className="text-slate-600 text-sm">
+                    {formatDate(expense.date!.toString())}
+                </p>
+            </div>
+            <AmountDisplay amount={expense.amount} />
+        </div>
+    )
 }
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -4,3 +4,14 @@ export function formatCurrency(amount: number) {
         currency: "USD",
     }).format(amount)
 }
+
+export function formatDate(dateString: string): string {
+    const dateObject = new Date(dateString)
+    const options: Intl.DateTimeFormatOptions = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    }
+    return new Intl.DateTimeFormat("es-ES", options).format(dateObject)
+}
